04406781980605a6bf8b9a4d7e115c5a115e5207
Janmeon <janemon2331@gmail.com>
2019-05-22 22:25:23 +0800

    update for reinstall system

---
+++ C/hello_world/helloWorld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>              // 头文件: 除了核心代码外的辅助功能
+
+int main(){                     // 函数返回值为整型; 程序(不是某个源代码文件, 而是多个源文件组成的一个程序)只能有一个主函数
+    printf("hello World!\n");   // print format, 格式化输出
+    return 0;                   // 返回值，代表函数的结束
+}
---
+++ C/system.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+int main(){
+    system("mocp");
+    return 0;
+}
+/*
+   int system(const char * string)
+   是可以调用系统命令的函数，成功返回0，错误返回1或其他数字
+*/
